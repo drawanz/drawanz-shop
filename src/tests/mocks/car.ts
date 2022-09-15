92444fbfdc75629aacc4198966990ad123ad2da2
Leonidas <leonidas.drawanz@gmail.com>
2022-09-15 19:37:11 -0300

    feat: testa a função create da model

--- src/tests/mocks/car.ts
+++ src/tests/mocks/car.ts
@@ -9,9 +9,9 @@ export const carMockCreate: ICar = {
   buyValue: 3500000,
   doorsQty: 2,
   seatsQty: 2,
-}
+};
 
-export const carMockRead: ICar & { _id: string } = {
+export const carMockCreateReturn: ICar & { _id: string } = {
   model: "Ferrari Maranello",
   year: 1963,
   color: "red",
@@ -19,4 +19,4 @@ export const carMockRead: ICar & { _id: string } = {
   doorsQty: 2,
   seatsQty: 2,
   _id: "4edd40c86762e0fb12000003"
-}
+};
--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -1,19 +1,28 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
-import { idMock, carMockRead, carMockCreate } from '../../mocks/car';
+import { idMock, carMockCreateReturn, carMockCreate } from '../../mocks/car';
 import { ErrorTypes } from '../../../erros/catalog';
 import CarModel from '../../../models/Cars';
 
 describe('Testes de Car para camada model', () => {
   const carModel = new CarModel();
 
-  describe('Verifica a função read', () => {
-    it('Função read executada com sucesso', async () => {
-      sinon.stub(Model, 'create').resolves(carMockRead);
+  describe('Verifica a função create', () => {
+    it('Função create executada com sucesso', async () => {
+      sinon.stub(Model, 'create').resolves(carMockCreateReturn);
       const response = await carModel.create(carMockCreate);
-      expect(response).to.be.equal(carMockRead);
+      expect(response).to.be.deep.equal(carMockCreateReturn);
       sinon.restore();
     });
-  })
+  });
+
+  describe('Verifica a função read', () => {
+    it('Função read executada com sucesso', async () => {
+      sinon.stub(Model, 'find').resolves([carMockCreateReturn]);
+      const response = await carModel.read();
+      expect(response).to.be.deep.equal([carMockCreateReturn]);
+      sinon.restore();
+    });
+  });
 })
